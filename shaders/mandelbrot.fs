5cfe650ee0016424147bf9ec58a0395a463de305
pavlovator <filip.pavlove@optimaideas.com>
2025-02-24 13:59:47 +0100

    shader start

---
+++ shaders/mandelbrot.fs
@@ -0,0 +1,13 @@
+#version 330
+
+// check
+// https://github.com/raysan5/raylib/blob/master/examples/shaders/shaders_julia_set.c
+// https://github.com/raysan5/raylib/blob/master/examples/shaders/resources/shaders/glsl330/julia_set.fs
+// or
+// https://github.com/raysan5/raylib/blob/master/examples/shaders/shaders_multi_sample2d.c
+// https://github.com/raysan5/raylib/blob/master/examples/shaders/resources/shaders/glsl330/color_mix.fs
+
+
+void main() {
+
+}
