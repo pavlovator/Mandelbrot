8b7d8df8a12e92e7456d2e3f27390458eaa30af2
pavlovator <pavlove.filip@gmail.com>
2025-02-24 21:58:57 +0100

    shader impl

--- main.cpp
+++ main.cpp
@@ -3,98 +3,82 @@
 #include <cmath>
 #include <iostream>
 
-// use GLSL shader!!!
+#if defined(PLATFORM_DESKTOP)
+    #define GLSL_VERSION            330
+#else   // PLATFORM_ANDROID, PLATFORM_WEB
+    #define GLSL_VERSION            100
+#endif
+
+
 Vector2 r_axis{-2.0f, 1.0f};
 Vector2 i_axis{-1.5f, 1.5f};
-const float stability_threshold = 2.0f;
-const float zoom_factor = 0.01f;
-
-
-bool isStable(Vector2& C) {
-    Vector2 zn{0.0f, 0.0f};
-    Vector2 zn_1{0.0f, 0.0f};
-    const int iterations = 20;
-    
-    // can optimize this if zn is smaller then 2 get out of cycle prematurely
-    for (int i=0; i<iterations; i++) {
-        // zn+1 = zn^2 + C
-        // (a+bi)(a+bi) = (a^2-b^2) + (2ab)i
-        Vector2 square_zn{
-            zn.x*zn.x - zn.y*zn.y,
-            2.0f*zn.x*zn.y
-        };
-        zn_1 = Vector2Add(square_zn, C);
-        zn = zn_1;
-        if (Vector2Length(zn) > stability_threshold) {
-            return false;
-        }
-    }
-    if (Vector2Length(zn) > stability_threshold) {
-        return false;
-    } 
-    return true;
-}
-
-void drawMandelbrot(int width, int height) {
-    for (int x=0; x<width; x++) {
-        for (int y=0; y<height; y++) {
-
-            Vector2 C{
-                ((x*1.0f / width) * abs(r_axis.x - r_axis.y)) + r_axis.x,
-                ((y*1.0f / height) * abs(i_axis.x - i_axis.y)) + i_axis.x
-            };
-            if (isStable(C)) {
-                DrawPixel(x, y, BLACK);
-            } else {
-                DrawPixel(x, y, WHITE);
-            }
-        }
-    }
-}
+float zoom_factor = 0.01f;
 
 int main() {
-    Color GREY = {29, 29, 29, 255};
-
     const int WINDOW_WIDTH = 800;
     const int WINDOW_HEIGHT = 800;
     int FPS = 30;
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Mandelbrot set");
+    
+    Shader shader = LoadShader(0, TextFormat("../shaders/mandelbrot.fs", GLSL_VERSION));
+    float r_offset[2] = { -2.0f, 1.0f };
+    float i_offset[2] = { -1.5f, 1.5f };
+
+    int r_offset_loc = GetShaderLocation(shader, "r_offset");
+    int i_offset_loc = GetShaderLocation(shader, "i_offset");
+
+    SetShaderValue(shader, r_offset_loc, r_offset, SHADER_UNIFORM_VEC2);
+    SetShaderValue(shader, i_offset_loc, i_offset, SHADER_UNIFORM_VEC2);
+
     SetTargetFPS(FPS);
+
+
     // Simulation Loop
     while (!WindowShouldClose()) {
         // 1. Event Handling
         if (IsKeyDown(KEY_PAGE_DOWN)) {
-            r_axis.x *= (1-zoom_factor);
-            r_axis.y *= (1-zoom_factor);
-            i_axis.x *= (1-zoom_factor);
-            i_axis.y *= (1-zoom_factor);
+            r_offset[0] -= zoom_factor;
+            r_offset[1] += zoom_factor;
+            i_offset[0] -= zoom_factor;
+            i_offset[1] += zoom_factor;
+        }
+        if (IsKeyDown(KEY_PAGE_UP)) {
+            r_offset[0] += zoom_factor;
+            r_offset[1] -= zoom_factor;
+            i_offset[0] += zoom_factor;
+            i_offset[1] -= zoom_factor;
+        }
+        if (IsKeyDown(KEY_RIGHT)) {
+            r_offset[0] += zoom_factor;
+            r_offset[1] += zoom_factor;
+        }
+        if (IsKeyDown(KEY_LEFT)) {
+            r_offset[0] -= zoom_factor;
+            r_offset[1] -= zoom_factor;
+        }
+        // glsl has 0,0 at bottom left 
+        if (IsKeyDown(KEY_UP)) {
+            i_offset[0] += zoom_factor;
+            i_offset[1] += zoom_factor;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            i_offset[0] -= zoom_factor;
+            i_offset[1] -= zoom_factor;
+        }
 
-        } else if (IsKeyDown(KEY_PAGE_UP)) {
-            r_axis.x *= (1+zoom_factor);
-            r_axis.y *= (1+zoom_factor);
-            i_axis.x *= (1+zoom_factor);
-            i_axis.y *= (1+zoom_factor);
-        } else if (IsKeyDown(KEY_RIGHT)) {
-            r_axis.x += zoom_factor;
-            r_axis.y += zoom_factor;
-        } else if (IsKeyDown(KEY_LEFT)) {
-            r_axis.x -= zoom_factor;
-            r_axis.y -= zoom_factor;
-        } else if (IsKeyDown(KEY_UP)) {
-            i_axis.x -= zoom_factor;
-            i_axis.y -= zoom_factor;
-        }  else if (IsKeyDown(KEY_DOWN)) {
-            i_axis.x += zoom_factor;
-            i_axis.y += zoom_factor;
-        } 
+        SetShaderValue(shader, r_offset_loc, r_offset, SHADER_UNIFORM_VEC2);
+        SetShaderValue(shader, i_offset_loc, i_offset, SHADER_UNIFORM_VEC2);
 
         // 2. Updating State
         
         // 3. Drawing Objects
         BeginDrawing();
-	    ClearBackground(GREY);
-        drawMandelbrot(WINDOW_WIDTH, WINDOW_HEIGHT);
-        DrawText(TextFormat("Target FPS: %i \nCurrent FPS: %i", FPS, GetFPS()), 20, 20, 30, RED);
+	        ClearBackground(WHITE);
+
+            BeginShaderMode(shader);  // Begin using the shader
+            DrawRectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, WHITE);  // Draw a full screen white rectangle
+            EndShaderMode();  // End using the shader
+            DrawText(TextFormat("Target FPS: %i \nCurrent FPS: %i", FPS, GetFPS()), 20, 20, 30, RED);
         EndDrawing();
     }    
 
--- shaders/mandelbrot.fs
+++ shaders/mandelbrot.fs
@@ -1,4 +1,4 @@
-#version 330
+#version 330 core
 
 // check
 // https://github.com/raysan5/raylib/blob/master/examples/shaders/shaders_julia_set.c
@@ -7,7 +7,41 @@
 // https://github.com/raysan5/raylib/blob/master/examples/shaders/shaders_multi_sample2d.c
 // https://github.com/raysan5/raylib/blob/master/examples/shaders/resources/shaders/glsl330/color_mix.fs
 
+uniform vec2 r_offset;
+uniform vec2 i_offset;
+
+out vec4 finalColor;
 
 void main() {
+    // Get the current fragment's coordinates
+    vec2 pix_coords = gl_FragCoord.xy;
+    vec2 zn = vec2(0.0, 0.0);
+    vec2 zn_1 = vec2(0.0, 0.0);
+    int iterations = 20;
+    bool is_out = false;
 
+    float WINDOW_WIDTH = 800.0;
+    float WINDOW_HEIGHT = 800.0;
+
+    // normalize for plane described by r_offset for real part and i_offset for imaginary part
+    vec2 c = vec2(((pix_coords.x / WINDOW_WIDTH)*abs(r_offset.x - r_offset.y))+r_offset.x,
+                  ((pix_coords.y / WINDOW_HEIGHT)*abs(i_offset.x - i_offset.y))+i_offset.x);
+
+    float iterations_out = 0.0;
+    for (int i=0; i<iterations; i++) {
+        // we want to compute zn+1 = zn^2 + C
+        zn_1 = vec2(zn.x*zn.x - zn.y*zn.y, 2*zn.x*zn.y) + c;
+        zn = zn_1;
+        if (length(zn) > 2) {
+            is_out = true;
+            iterations_out = i;
+            break;
+        }
+    }
+
+    if (is_out) {
+        finalColor = vec4(1.0/iterations_out, 1.0/iterations_out, 1.0/iterations_out, 1.0);
+    } else {
+        finalColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
 }
